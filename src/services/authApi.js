fd7b01019daf0225af9d09344416d1cb4a561da3
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2021-03-30 00:21:33 +0300

    auth template create

---
+++ src/services/authApi.js
@@ -0,0 +1,31 @@
+// import axios from 'axios';
+// axios.defaults.baseURL = 'https://goit-phonebook-api.herokuapp.com';
+
+// export const authToken = {
+//   set(token) {
+//     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+//   },
+//   unset() {
+//     axios.defaults.headers.common.Authorization = '';
+//   },
+// };
+
+// // AUTH
+// export const signup = contact => {
+//   return axios.post('/users/signup', contact).then(({ data }) => data);
+// };
+
+// // AUTH
+// export const login = contact => {
+//   return axios.post('/users/login', contact).then(({ data }) => data);
+// };
+
+// // AUTH
+// export const logout = () => {
+//   return axios.post('/users/logout').then(({ data }) => data);
+// };
+
+// // AUTH
+// export const getUser = () => {
+//   return axios.get('/users/current').then(({ data }) => data);
+// };
