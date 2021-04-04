214c62fc65666a2ffb70d6ec050ef947edea60d8
vovababich <vova_babich@list.ru>
2021-04-04 18:39:40 +0300

    saved

--- src/components/Test/Card.js
+++ src/components/Test/Card.js
@@ -24,59 +24,15 @@ export default function Card({ questions, activeCard, handleAnswer, answered }) 
 
       <RadioGroup name="question" value={answered[questionId] || ''} onChange={handleChange}>
         {answers.map((answer, i) => (
-          <FormControlLabel key={i} value={answer} control={<Radio />} label={answer} />
+          <FormControlLabel
+            className={s.answer}
+            key={i}
+            value={answer}
+            control={<Radio />}
+            label={answer}
+          />
         ))}
       </RadioGroup>
     </div>
   );
 }
-
-// import { makeStyles } from '@material-ui/core/styles';
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
-// import Typography from '@material-ui/core/Typography';
-// import RadioButtonsGroup from './Radio';
-// const useStyles = makeStyles({
-//   root: {
-//     minWidth: 275,
-//   },
-//   bullet: {
-//     display: 'inline-block',
-//     margin: '0 2px',
-//     transform: 'scale(0.8)',
-//   },
-//   title: {
-//     fontSize: 14,
-//   },
-//   pos: {
-//     marginBottom: 12,
-//   },
-// });
-
-// export default function SimpleCard({ testLength, numOfTest, question, answers, handleAnswer, id }) {
-//   const classes = useStyles();
-//   const bull = <span className={classes.bullet}>•</span>;
-
-//   return (
-//     <>
-//       <Card className={classes.root}>
-//         <CardContent>
-//           <Typography className={classes.title} color="textSecondary" gutterBottom>
-//             Вопрос {numOfTest} / {testLength}
-//           </Typography>
-//           <Typography variant="h5" component="h2" className={classes.title}>
-//             {question}
-//           </Typography>
-//           <RadioButtonsGroup answers={answers} handleAnswer={handleAnswer} id={id} />
-//         </CardContent>
-//       </Card>
-//       {/* <div class="card">
-//         <p class="counter">
-//           Вопрос {numOfTest} / {testLength}
-//         </p>
-//         <h2 class="question"> {question}</h2>
-//         <RadioButtonsGroup answers={answers} handleAnswer={handleAnswer} id={id} />
-//       </div> */}
-//     </>
-//   );
-// }
--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -6,11 +6,12 @@ import s from './Test.module.css';
 export default function Test({ title }) {
   const [answers, setAnswers] = useState({});
   const [activeCard, setActiveCard] = useState(0);
+
+  useEffect(() => {}, []);
+
   const handlePrev = () => {
     setActiveCard(activeCard - 1);
   };
-
-  useEffect(() => {}, []);
 
   const handleNext = () => {
     setActiveCard(activeCard + 1);
@@ -20,12 +21,14 @@ export default function Test({ title }) {
     setAnswers({ ...answers, ...targerAnswer });
   };
 
+  const handleMainButton = () => {};
+
   return (
     <>
-      <div className={s.wrapper}>
+      <section className={s.section}>
         <div className={s.above}>
           <h2 className={s.title}>{title ? title : '[Теория тестирования_]'}</h2>
-          <button className={s.aboveButton} type="button">
+          <button className={s.aboveButton} type="button" onClick={handleMainButton}>
             Завершить тест
           </button>
         </div>
@@ -51,9 +54,6 @@ export default function Test({ title }) {
             >
               <path d="M0.146485 8.35362L7.64648 15.8536C7.84181 16.049 8.15822 16.049 8.3535 15.8536C8.54883 15.6583 8.54883 15.3419 8.3535 15.1466L1.707 8.50011L23.5 8.50011C23.7764 8.50011 24 8.27647 24 8.00009C24 7.72372 23.7764 7.50008 23.5 7.50008L1.707 7.50008L8.3535 0.853625C8.54883 0.658297 8.54883 0.341892 8.3535 0.146611C8.25586 0.0489698 8.12789 0.000126405 7.99997 0.000126394C7.87205 0.000126382 7.74413 0.0489698 7.64644 0.146611L0.146437 7.64661C-0.0488427 7.84189 -0.0488427 8.1583 0.146485 8.35362Z" />
             </svg>
-            {/* <svg class="arrow" width="24" height="16">
-              <use href="./sprite.svg#prev"></use>
-            </svg> */}
           </button>
           <button
             className={s.btn}
@@ -70,31 +70,9 @@ export default function Test({ title }) {
             >
               <path d="M23.8535 7.64662L16.3535 0.146618C16.1582 -0.04871 15.8418 -0.04871 15.6465 0.146618C15.4512 0.341946 15.4512 0.658353 15.6465 0.853634L22.293 7.50013L0.500016 7.50013C0.223641 7.50013 0 7.72377 0 8.00015C0 8.27652 0.223641 8.50016 0.500016 8.50016L22.293 8.50016L15.6465 15.1466C15.4512 15.3419 15.4512 15.6584 15.6465 15.8536C15.7441 15.9513 15.8721 16.0001 16 16.0001C16.128 16.0001 16.2559 15.9513 16.3536 15.8536L23.8536 8.35363C24.0488 8.15835 24.0488 7.84195 23.8535 7.64662Z" />
             </svg>
-            {/* <svg class="arrow" width="24" height="16">
-              <use href="./sprite.svg#next"></use>
-            </svg> */}
           </button>
         </div>
-      </div>
+      </section>
     </>
   );
 }
-
-// import PreviousNextMethods from './Slider';
-// export default function Test({ title }) {
-//   const [answers, setAnswers] = useState({});
-//   console.log(`answers`, answers);
-//   //   console.log('длина', Object.keys(answers).length);
-
-//   // [{ id: 4, answer: 'dcdsc' }];
-//   const handleAnswer = targerAnswer => {
-//     console.log(`targerAnswer`, targerAnswer);
-//     setAnswers({ ...answers, ...targerAnswer });
-//   };
-//   return (
-//     <>
-//       <h2>{title ? title : '[Теория тестирования_]'}</h2>
-//       <PreviousNextMethods handleAnswer={handleAnswer} />
-//     </>
-//   );
-// }
