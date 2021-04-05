43af686a5cc71dfaa7516442297287b1f8ba5675
Grigoriy Chernish <cga.jsdev@gmail.com>
2021-04-05 12:42:34 +0300

    add routs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, Suspense, lazy } from 'react';
 import { Switch } from 'react-router-dom';
+import Navigation from './components/Navigation';
 
 import PreLoader from './components/PreLoader';
 
@@ -13,15 +14,24 @@ const AuthPage = lazy(() => import('pages/AuthPage/AuthPage' /* webpackChunkName
 function App() {
   return (
     <>
+      <Navigation />
       <Suspense fallback={<PreLoader sizePreloader="200px" />}>
         <Switch>
-          <PublicRoute exact path="/auth">
+          <PublicRoute path="/auth" redirectTo="/" restricted>
             <AuthPage />
           </PublicRoute>
 
-          <PrivateRoute path="/" redirectTo="/auth">
+          <PrivateRoute exact path="/" redirectTo="/auth">
             <MainPage />
           </PrivateRoute>
+
+          <PrivateRoute path="/materials" redirectTo="/auth">
+            <div>Страница доп материалов</div>
+          </PrivateRoute>
+
+          <PublicRoute path="/contacts">
+            <div>Contacts</div>
+          </PublicRoute>
 
           <PublicRoute>
             <div>not found</div>
