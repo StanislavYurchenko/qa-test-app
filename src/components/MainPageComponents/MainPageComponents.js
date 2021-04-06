d26ae3a7c8af60e61c7465d44700efdef9536110
Grigoriy Chernish <cga.jsdev@gmail.com>
2021-04-06 16:08:22 +0300

    create structure mainPage

---
+++ src/components/MainPageComponents/MainPageComponents.js
@@ -0,0 +1,23 @@
+function MainPageComponents() {}
+
+export default MainPageComponents;
+
+// import { useDispatch } from 'react-redux';
+// import { logoutUser } from '../../redux/auth/authOperations';
+
+// const MainPage = () => {
+//   const dispatch = useDispatch();
+
+//   const logOut = () => {
+//     dispatch(logoutUser());
+//   };
+
+//   return (
+//     <>
+//       <h1>Home page</h1>
+//       <button onClick={logOut}>Logout</button>
+//     </>
+//   );
+// };
+
+// export default MainPage;
---
+++ src/components/MainPageComponents/MainText/MainText.js
@@ -0,0 +1,3 @@
+function MainText() {}
+
+export default MainText;
