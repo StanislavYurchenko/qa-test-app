5ba1c2f7e75a7395bccc03209eb37d5449cc11ff
Grigoriy Chernish <cga.jsdev@gmail.com>
2021-04-06 22:56:11 +0300

    check point )

--- src/components/MainPageComponents/MainPageComponents.js
+++ src/components/MainPageComponents/MainPageComponents.js
@@ -1,23 +1,21 @@
-function MainPageComponents() {}
+import { useDispatch } from 'react-redux';
+import { logoutUser } from '../../redux/auth/authOperations';
+import { Paper, Container, Typography } from '@material-ui/core';
+
+function MainPageComponents() {
+  const dispatch = useDispatch();
+
+  const logOut = () => {
+    dispatch(logoutUser());
+  };
+  return (
+    <main className="main">
+      <h1>
+        “Regression testing. What is it? If the system compiles, that's good, if it boots, that's
+        great!”
+      </h1>
+    </main>
+  );
+}
 
 export default MainPageComponents;
-
-// import { useDispatch } from 'react-redux';
-// import { logoutUser } from '../../redux/auth/authOperations';
-
-// const MainPage = () => {
-//   const dispatch = useDispatch();
-
-//   const logOut = () => {
-//     dispatch(logoutUser());
-//   };
-
-//   return (
-//     <>
-//       <h1>Home page</h1>
-//       <button onClick={logOut}>Logout</button>
-//     </>
-//   );
-// };
-
-// export default MainPage;
--- src/components/MainPageComponents/MainText/MainText.js
+++
@@ -1,3 +0,0 @@
-function MainText() {}
-
-export default MainText;
