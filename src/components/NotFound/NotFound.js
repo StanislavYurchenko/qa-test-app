913a89dc191a9ce403d753ac87e42d8d28e2a056
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2021-04-06 23:36:55 +0300

    not-found start

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,7 +1,12 @@
-import Container from '../Container';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-  return <p>not found</p>;
+  return (
+    <>
+      <p>not found</p>
+      <Link to="/"> home </Link>
+    </>
+  );
 };
 
 export default NotFound;
