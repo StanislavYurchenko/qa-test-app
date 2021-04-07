46054f86efeb367611d5ea93f71284465619d8b4
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2021-04-07 23:20:04 +0300

    pull dev

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import { Container, Oops } from './NotFound.style';
 
 const NotFound = () => {
   return (
-    <>
-      <p>not found</p>
-      <Link to="/"> home </Link>
-    </>
+    <Container>
+      <RemoveCircleIcon />
+      <Oops>OOPS!!</Oops>
+      <div>We can't seems to find the page you're looking for</div>
+      <div>
+        ERROR code: <span>404</span>
+      </div>
+      <div>
+        <span>return to </span>
+        <Link to="/">main</Link>
+        <span> page</span>
+      </div>
+    </Container>
   );
 };
 
---
+++ src/components/NotFound/NotFound.style.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import styled from 'styled-components';
+import * as colours from '../../themes/colors';
+
+export const Container = styled(Box)``;
+
+export const Oops = styled.div`
+  font-size: 3rem;
+`;
