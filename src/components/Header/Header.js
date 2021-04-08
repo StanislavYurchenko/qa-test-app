3371a655084f27a1c13f5790cf15f527b4537823
Andriy <61834912+Andriy1982@users.noreply.github.com>
2021-04-08 22:03:38 +0300

    Header (#23)
    * add branch header
    
    * add new styles
    
    * rename images
    
    * before marge
    
    * add logout
    
    * add navigation
    
    * add navigation
    
    * add button logout
    
    * add container
    
    * add container
    
    * new task
    
    * refactor styles
    
    * add breakpoints
    
    * add userInfo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,33 +4,29 @@ import styled from 'styled-components';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Navigation from 'components/Navigation';
-import { MainContainer } from 'components/Container/Container.style';
 import { ReactComponent as LogoSvg } from '../../images/header_icons/logo.svg';
 import { ReactComponent as OpenMenuSvg } from '../../images/header_icons/openMenu.svg';
 import { ReactComponent as CloseMenuSvg } from '../../images/header_icons/closeMenu.svg';
 import { ReactComponent as LogOut } from '../../images/header_icons/signOut.svg';
-import { Modal, HeaderWrap, ButtonWrap, Logo, ButtonStyles, Span } from './Header.style';
+import { Modal, HeaderWrap, ButtonWrap, Logo, ButtonStyles, Span, UserName } from './Header.style';
 import { useStyles } from './Header.style';
-import { getIsLoggedIn } from '../../redux/auth/authSelectors';
+import { getIsLoggedIn, getUserName } from '../../redux/auth/authSelectors';
 import { logoutUser } from '../../redux/auth/authOperations';
-
-const TestNav = styled.p`
-  color: black;
-  font-size: 14px;
-`;
 
 export default function Header() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const isLogin = useSelector(getIsLoggedIn);
+  const userName = useSelector(getUserName);
   const dispatch = useDispatch();
   const classes = useStyles();
+
+  const avatarLetter = userName?.slice(0, 1).toUpperCase();
 
   const onButtonClick = () => {
     setIsModalOpen(!isModalOpen);
   };
 
   return (
-    // <MainContainer>
     <HeaderWrap>
       <AppBar position="static" className={classes.headerStyles}>
         <Toolbar className={classes.toolBarStyles}>
@@ -38,8 +34,11 @@ export default function Header() {
             <LogoSvg />
           </Logo>
           <Navigation className={classes.TestNav} />
-          {/* <TestNav className={classes.TestNav}>Contacts</TestNav> */}
-          {isLogin && <Span>D</Span>}
+          {isLogin && (
+            <>
+              <Span>{avatarLetter}</Span> <UserName>{userName}</UserName>
+            </>
+          )}
 
           {isLogin && (
             <ButtonWrap>
@@ -66,6 +65,5 @@ export default function Header() {
         )}
       </Modal>
     </HeaderWrap>
-    //  </MainContainer>
   );
 }
