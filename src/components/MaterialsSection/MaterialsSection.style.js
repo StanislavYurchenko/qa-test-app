ebb7c67e8e105cd8875ab4328f919dc1e4242b79
Elena Panasyuk <63494893+elenapanasyuk@users.noreply.github.com>
2021-04-09 21:27:45 +0300

    Добавляет стили и фон (#27)

--- src/components/MaterialsSection/MaterialsSection.style.js
+++ src/components/MaterialsSection/MaterialsSection.style.js
@@ -4,41 +4,54 @@ import {
   PAGE_BORDER_COLOUR,
   PAGE_BACKGROUND_COLOUR,
 } from '../../themes/colors';
+import { background } from '../../images/background/background';
+import BREAKPOINT from '../../utils/breakpoints';
 
 export const SectionStyle = styled.section`
-  margin: 0;
+  position: relative;
+  top: 70px;
+  margin: 0 auto;
   text-align: left;
+  min-height: 100vh;
   background-color: ${PAGE_BACKGROUND_COLOUR};
-  padding: 40px 20px 56px;
-  background-image: url();
-
-  @media screen and (min-width: 768px) {
-    padding: 41px 22px 76px;
+  padding: 40px 20px;
+  background-image: url(${background.bg_mob});
+  background-size: cover;
+  repeat: no-repeat;
+  @media screen and (min-device-pixel-ratio: 2),
+  screen and (-webkit-min-device-pixel-ratio: 2),
+  screen and (-o-min-device-pixel-ratio: 2/1),
+  screen and (min-resolution: 192dpi),
+  screen and (min-resolution: 2dppx) {
+  {
+    background-image: url(${background.bg_mob2x});
   }
 
-  @media screen and (min-width: 1290px) {
-    padding: 40px 20px 65px;
+  @media screen and (min-width: ${BREAKPOINT.TABLET}) {
+    padding: 119px 115px;
+    background-image: url(${background.bg_tab});
+
+@media screen and (min-device-pixel-ratio: 2),
+  screen and (-webkit-min-device-pixel-ratio: 2),
+  screen and (-o-min-device-pixel-ratio: 2/1),
+  screen and (min-resolution: 192dpi),
+  screen and (min-resolution: 2dppx) {
+  {
+    background-image: url(${background.bg_tab2x});
   }
-`;
+  }
 
-export const Title = styled.h2`
-  font-family: Montserrat, sans-serif;
-  font-weight: 700;
-  font-size: 18px;
-  line-height: 30px;
-  text-align: left;
-  letter-spacing: 0.02em;
-  color: ${PRIMARY_TEXT_COLOUR};
-  border-bottom: 1px solid ${PAGE_BORDER_COLOUR};
-  width: 130px;
-  margin: 0;
-  padding-bottom: 10px;
-  margin-bottom: 20px;
+  @media screen and (min-width: ${BREAKPOINT.DESKTOP}) {
+    padding: 98px 190px;
+    background-image: url(${background.bg_desk});
 
-  @media screen and (min-width: 768px) {
-    font-size: 22px;
-    width: 322px;
-    padding-bottom: 22px;
-    margin-bottom: 39px;
+  @media screen and (min-device-pixel-ratio: 2),
+  screen and (-webkit-min-device-pixel-ratio: 2),
+  screen and (-o-min-device-pixel-ratio: 2/1),
+  screen and (min-resolution: 192dpi),
+  screen and (min-resolution: 2dppx) {
+  {
+    background-image: url(${background.bg_desk2x});
+  }
   }
 `;
