0e32b4fafc82c27679c1b47bf6930f022c4bb1ca
YuliaLabutkina <labutkinayuliaa@gmail.com>
2021-04-12 02:39:52 +0300

    final-fix

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -5,20 +5,23 @@ import { useSelector } from 'react-redux';
 import Container from '../Container';
 import Test from '../Test';
 import { authSelectors } from 'redux/auth';
-import { getQuestions } from '../../redux/test/testSelectors';
+import { getQuestions, getAnswers } from '../../redux/test/testSelectors';
+import { transformAnswers } from '../../services/transformAnswers';
 
 function PrivateRoute({ children, redirectTo, ...routeProps }) {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   const questions = useSelector(getQuestions);
+  const answers = useSelector(getAnswers);
+  const isRenderTest =
+    questions.length > 0 && questions.length !== transformAnswers(answers).length;
 
-  const page =
-    questions.length > 0 ? (
-      <Container>
-        <Test />
-      </Container>
-    ) : (
-      children
-    );
+  const page = isRenderTest ? (
+    <Container>
+      <Test />
+    </Container>
+  ) : (
+    children
+  );
 
   return <Route {...routeProps}>{isLoggedIn ? page : <Redirect to={redirectTo} />}</Route>;
 }
