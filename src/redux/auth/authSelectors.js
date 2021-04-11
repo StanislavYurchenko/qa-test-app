195168b0b7fdaa2b5689cd8e1ca410c15f87349c
RomanVoznyy <roman.voznyy.52@gmail.com>
2021-04-11 17:31:35 +0300

    created base logic of refresh_token

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -3,3 +3,4 @@ export const getIsLoggedIn = state => state.auth.isLoggedIn;
 export const isSuccessfulReg = state => state.auth.successfulReg;
 export const loading = state => state.auth.loading;
 export const error = state => state.auth.error;
+export const getTokenData = state => state.auth.token;
