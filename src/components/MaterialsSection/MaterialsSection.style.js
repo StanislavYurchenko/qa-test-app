22b3096f8b4d1391429706d0b682030bb34a9ac7
elenapanasyuk <elena.panasyuk@gmail.com>
2021-04-11 20:42:34 +0300

    Background pictures were updated

--- src/components/MaterialsSection/MaterialsSection.style.js
+++ src/components/MaterialsSection/MaterialsSection.style.js
@@ -1,9 +1,4 @@
 import styled from 'styled-components';
-import {
-  PRIMARY_TEXT_COLOUR,
-  PAGE_BORDER_COLOUR,
-  PAGE_BACKGROUND_COLOUR,
-} from '../../themes/colors';
 import { background } from '../../images/background/background';
 import BREAKPOINT from '../../utils/breakpoints';
 
@@ -13,45 +8,35 @@ export const SectionStyle = styled.section`
   margin: 0 auto;
   text-align: left;
   min-height: 100vh;
-  background-color: ${PAGE_BACKGROUND_COLOUR};
   padding: 40px 20px;
   background-image: url(${background.bg_mob});
   background-size: cover;
-  repeat: no-repeat;
-  @media screen and (min-device-pixel-ratio: 2),
-  screen and (-webkit-min-device-pixel-ratio: 2),
-  screen and (-o-min-device-pixel-ratio: 2/1),
-  screen and (min-resolution: 192dpi),
-  screen and (min-resolution: 2dppx) {
-  {
-    background-image: url(${background.bg_mob2x});
-  }
+  background-repeat: no-repeat;
+  background-position: right top;
 
   @media screen and (min-width: ${BREAKPOINT.TABLET}) {
     padding: 119px 115px;
     background-image: url(${background.bg_tab});
-
-@media screen and (min-device-pixel-ratio: 2),
-  screen and (-webkit-min-device-pixel-ratio: 2),
-  screen and (-o-min-device-pixel-ratio: 2/1),
-  screen and (min-resolution: 192dpi),
-  screen and (min-resolution: 2dppx) {
-  {
-    background-image: url(${background.bg_tab2x});
-  }
   }
 
   @media screen and (min-width: ${BREAKPOINT.DESKTOP}) {
     padding: 98px 190px;
     background-image: url(${background.bg_desk});
-
-  @media screen and (min-device-pixel-ratio: 2),
-  screen and (-webkit-min-device-pixel-ratio: 2),
-  screen and (-o-min-device-pixel-ratio: 2/1),
-  screen and (min-resolution: 192dpi),
-  screen and (min-resolution: 2dppx) {
-  {
-    background-image: url(${background.bg_desk2x});
   }
+
+  @media (-webkit-min-device-pixel-ratio: 2) and (max-width: ${BREAKPOINT.MOBILE_MAX}),
+    (-o-min-device-pixel-ratio: 2/1) and (max-width: ${BREAKPOINT.MOBILE_MAX}),
+    (min-resolution: 192dpi) and (max-width: ${BREAKPOINT.MOBILE_MAX}) {
+    background-image: url(${background.bg_mob2x});
+  }
+  @media (-webkit-min-device-pixel-ratio: 2) and (min-width: ${BREAKPOINT.TABLET}),
+    (-o-min-device-pixel-ratio: 2/1) and (min-width: ${BREAKPOINT.TABLET}),
+    (min-resolution: 192dpi) and (min-width: ${BREAKPOINT.TABLET}) {
+    background-image: url(${background.bg_tab2x});
+  }
+  @media (-webkit-min-device-pixel-ratio: 2) and (min-width: ${BREAKPOINT.DESKTOP}),
+    (-o-min-device-pixel-ratio: 2/1) and (min-width: ${BREAKPOINT.DESKTOP}),
+    (min-resolution: 192dpi) and (min-width: ${BREAKPOINT.DESKTOP}) {
+    background-image: url(${background.bg_desk2x});
   }
 `;
--- src/images/background/background.js
+++ src/images/background/background.js
@@ -4,9 +4,6 @@ import bg_tab from './bg_tab.jpeg';
 import bg_tab2x from './bg_tab@2x.jpeg';
 import bg_mob from './bg_mob.jpeg';
 import bg_mob2x from './bg_mob@2x.jpeg';
-//import Yulia from './Yulia_Labutkina-min.jpg';
-//import Stanislav from './Yurchenko_Stanislav-min.jpg';
-//import Andriy from './Andriy_Krichkivskiy-min.jpg';
 
 export const background = {
   bg_desk,
@@ -15,12 +12,4 @@ export const background = {
   bg_tab2x,
   bg_mob,
   bg_mob2x,
-  //   Elena_Panasyuk: Elena,
-  //   Inna_Tishchenko: Inna,
-  //   Roman_Voznyy: Roman,
-  //   Valentin_Yakubovskiy: Valentin,
-  //   Volodymyr_Babych: Volodymyr,
-  //   Yulia_Labutkina: Yulia,
-  //Yurchenko_Stanislav: Stanislav,
-  //Andriy_Krichkivskiy: Andriy,
 };
