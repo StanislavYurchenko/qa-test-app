44a51b01530a03782463afee26b8ab0bcc53fe22
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2021-04-12 00:30:06 +0300

    hotfix toggle theme

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,28 +1,31 @@
 import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { ToggleContainer } from './Toggle.style.js';
 import { ReactComponent as MoonIcon } from '../../images/icons/moon-white.svg';
 import { ReactComponent as SunnyIcon } from '../../images/icons/sun-dark.svg';
 
-import { setThemeHandler } from '../../redux/theme/themeOperations';
+import { setThemeHandler, setNameTheme } from '../../redux/theme/themeOperations';
+import { getNameTheme } from '../../redux/theme/themeSelectors';
 
 const Toggle = () => {
-  const [theme, setTheme] = useState(true);
+  // const [theme, setTheme] = useState(true);
   const dispatch = useDispatch();
+  const nameTheme = useSelector(getNameTheme);
+  console.log('nameTheme', nameTheme);
 
-  // useEffect(() => {
-  //   dispatch(setThemeHandler('light'));
-  // }, []);
+  useEffect(() => {
+    dispatch(setThemeHandler(nameTheme));
+  }, [nameTheme]);
 
   const changeTheme = () => {
-    setTheme(!theme);
-    const nameTheme = theme ? 'light' : 'dark';
+    dispatch(setNameTheme(nameTheme === 'light' ? 'dark' : 'light'));
+    // const nameTheme = theme ? 'light' : 'dark';
     dispatch(setThemeHandler(nameTheme));
   };
 
   return (
-    <ToggleContainer lightTheme={theme} onClick={changeTheme}>
+    <ToggleContainer lightTheme={nameTheme === 'light'} onClick={changeTheme}>
       <SunnyIcon />
       <MoonIcon />
     </ToggleContainer>
--- src/redux/theme/themeAction.js
+++ src/redux/theme/themeAction.js
@@ -2,6 +2,9 @@ import { createAction } from '@reduxjs/toolkit';
 
 const setThemeSuccess = createAction('theme/toggleTheme');
 
+const setNameTheme = createAction('theme/setNameTheme');
+
 export default {
   setThemeSuccess,
+  setNameTheme,
 };
