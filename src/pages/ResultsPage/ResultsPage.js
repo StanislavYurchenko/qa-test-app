64af6b4a3a7875b52ec8dfa1afba2fbdbea1a19c
valentinyak <63512456+valentinyak@users.noreply.github.com>
2021-04-13 00:19:17 +0300

    code refactoring

--- src/pages/ResultsPage/ResultsPage.js
+++ src/pages/ResultsPage/ResultsPage.js
@@ -1,39 +1,5 @@
-import Button from '@material-ui/core/Button';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import ResultsComponent from '../../components/ResultsComponent';
 
-import { MainContainer, Header1, Header2, StyledImage, useStyles } from './ResultsPage.style';
-import Chart from '../../components/Chart';
-import TestResult from '../../components/TestResult';
-import resultIMG from '../../images/results.svg';
-import testActions from '../../redux/test/testActions';
-import { getTheme } from '../../redux/theme/themeSelectors';
-
-export default function ResultsView() {
-  const theme = useSelector(getTheme);
-  const customTheme = createMuiTheme(theme);
-  const classes = useStyles(customTheme);
-  const testCategory = useSelector(state => state.test.category);
-  const dispatch = useDispatch();
-  const history = useHistory();
-
-  const handleButtonClick = () => {
-    dispatch(testActions.resetAnswers());
-    dispatch(testActions.resetResults());
-    history.push('/test');
-  };
-
-  return (
-    <MainContainer theme={customTheme}>
-      <Header1>Results</Header1>
-      <Header2>{testCategory}</Header2>
-      <Chart />
-      <StyledImage src={resultIMG} alt="cat result" />
-      <TestResult />
-      <Button className={classes.tryAgain} onClick={handleButtonClick}>
-        Try again
-      </Button>
-    </MainContainer>
-  );
+export default function ResultsPage() {
+  return <ResultsComponent />;
 }
