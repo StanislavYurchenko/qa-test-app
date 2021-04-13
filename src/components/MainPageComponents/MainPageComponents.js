19300eb00f3570feb9bb1bb991763e832a24e719
Andrii Krychkivskyi <kruchandriy@gmail.com>
2021-04-13 16:58:30 +0300

    start translation

--- src/components/MainPageComponents/MainPageComponents.js
+++ src/components/MainPageComponents/MainPageComponents.js
@@ -5,9 +5,14 @@ import { Section, Title, SecondTitle, Text, Button, List } from './MainPageCompo
 import { ReactComponent as ArrowSvg } from '../../images/icons/arrow.svg';
 import { useHistory } from 'react-router-dom';
 
+import { useTranslation } from 'react-i18next';
+import common_ru from '../../lang/en.json';
+
 function MainPageComponents() {
   const history = useHistory();
   const dispatch = useDispatch();
+
+  const { t } = useTranslation(['translation', 'common_ru']);
 
   const logOut = () => {
     dispatch(logoutUser());
@@ -26,8 +31,9 @@ function MainPageComponents() {
     <main>
       <Section>
         <Title>
-          “Regression testing. What is it? If the system compiles, that's good, if it boots, that's
-          great!”
+          {t('main.quote')}
+          {/* “Regression testing. What is it? If the system compiles, that's good, if it boots, that's
+          great!” */}
         </Title>
         <SecondTitle>Linus Torvalds</SecondTitle>
         <Text>Linux kernel creator, hacker, 1969</Text>
--- src/index.js
+++ src/index.js
@@ -9,13 +9,18 @@ import '@csstools/normalize.css';
 import './base.css';
 import App from './App';
 
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <I18nextProvider i18n={i18n}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </I18nextProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
