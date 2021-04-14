5aa896f11c163110ac185f3d42c8795a7d405775
Roman Voznyy <roman.voznyy.52@gmail.com>
2021-04-14 19:29:10 +0300

    Adminka (#56)
    * change getCurrent
    
    * change path

--- src/redux/test/testActions.js
+++ src/redux/test/testActions.js
@@ -7,6 +7,10 @@ const addCategory = createAction('test/addCategory');
 const addActiveCard = createAction('test/addActiveCard');
 const testRefresh = createAction('test/refresh');
 
+const testFetchRequest = createAction('test/testFetchRequest');
+const testFetchSuccess = createAction('test/testFetchSuccess');
+const testFetchError = createAction('test/testFetchError');
+
 export default {
   addAnswer,
   resetAnswers,
@@ -14,4 +18,7 @@ export default {
   addCategory,
   addActiveCard,
   testRefresh,
+  testFetchRequest,
+  testFetchSuccess,
+  testFetchError,
 };
--- src/redux/test/testOperations.js
+++ src/redux/test/testOperations.js
@@ -1,4 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+// import testActions from './testActions';
+// import { checkNeedsToUpdate } from '../auth/authOperations';
 import * as service from '../../services/authApi';
 
 export const fetchTest = createAsyncThunk('test/fetchTest', async (path, rejectWithValue) => {
@@ -9,6 +11,22 @@ export const fetchTest = createAsyncThunk('test/fetchTest', async (path, rejectW
     return rejectWithValue(error);
   }
 });
+
+// export const fetchTest = () => async (dispatch, getState) => {
+//   try {
+//     const response = await checkNeedsToUpdate(dispatch, getState);
+//     if (!response) {
+//       return;
+//     }
+
+//     dispatch(testActions.testFetchRequest());
+
+//     const res = await service.fetchQuestions(path);
+//     dispatch(testActions.testFetchSuccess(res));
+//   } catch (err) {
+//     dispatch(testActions.testFetchError(err.message));
+//   }
+// };
 
 export const sendAnswers = createAsyncThunk(
   'test/sendAnswers',
