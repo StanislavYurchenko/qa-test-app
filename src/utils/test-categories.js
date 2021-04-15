08150654ca5ff42c9cce85df2cf1a34953f084b0
Andrii Krychkivskyi <kruchandriy@gmail.com>
2021-04-15 21:05:16 +0300

    change file contacts

--- src/utils/test-categories.js
+++ src/utils/test-categories.js
@@ -1,1 +1,6 @@
 export default { theory: '[Testing theory_]', tech: '[QA technical training_]' };
+import i18n from '../i18n';
+
+export const getTheory = () => {
+  return { theory: i18n.t('results__description'), tech: i18n.t('qaTechnicalTraining') };
+};
