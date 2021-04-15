7d5bb8417e2706ea43938a9cfdca1125e8c8629b
Andrii Krychkivskyi <kruchandriy@gmail.com>
2021-04-15 22:56:31 +0300

    resolve conflict

--- src/i18n.js
+++ src/i18n.js
@@ -17,7 +17,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: 'ru',
+    lng: 'en',
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
