cb363b30fb45ec62393982906e93de2dc49f2738
Andrii Krychkivskyi <kruchandriy@gmail.com>
2021-04-15 17:01:41 +0300

    add translate contacts

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,14 +1,17 @@
 import ContactCard from '../ContactCard/';
-import contacts from '../../utils/contacts';
+import Contacts from '../../utils/contacts';
 import { List, ListItem } from './ContactsList.style.js';
 
 import { useSelector } from 'react-redux';
 import { getTheme } from '../../redux/theme/themeSelectors';
 import { createMuiTheme } from '@material-ui/core/styles';
+// console.log(Contacts());
 
 const ContactsList = () => {
   const theme = useSelector(getTheme);
   const customTheme = theme && createMuiTheme(theme);
+  const contacts = Contacts();
+  console.log(contacts);
 
   return (
     <List>
